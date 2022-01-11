c32360655b51ab40579d3a9406772b951655464c
Lucas Gillon <lucas.gillon98@gmail.com>
2022-01-11 16:58:56 +0100

    exo 11

--- 02_programming/01_variables/969830e3c3dda891a6b1aeed2709911232022bb1/11_play_with_join/src/index.js
+++ 02_programming/01_variables/969830e3c3dda891a6b1aeed2709911232022bb1/11_play_with_join/src/index.js
@@ -1,4 +1,6 @@
 // create another array called `litteralDigits` from `zero` to `nine` where each array entry is a spelled-out number;
-
-
+const litteralDigits = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+console.log(litteralDigits);
 // use `join` to create, into the variable `allDigits`, a string like this : `zero - one ...`.
+const allDigits = litteralDigits.join(" - ");
+console.log(allDigits);
