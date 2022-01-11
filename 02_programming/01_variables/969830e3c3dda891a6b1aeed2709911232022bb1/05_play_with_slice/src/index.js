c113a50b71756f14ea35076b0028366c664768be
Lucas Gillon <lucas.gillon98@gmail.com>
2022-01-11 14:31:43 +0100

    exercice 5

---
+++ 02_programming/01_variables/969830e3c3dda891a6b1aeed2709911232022bb1/05_play_with_slice/src/index.js
@@ -0,0 +1,7 @@
+const question = "spartan, what is your profession?!";
+
+// a constant `hobbyQuestion` bound to a string: "spartan, what is your hobby?!"
+// using `slice()` and the constant `question`
+const hobbyQuestion = (question.slice(0,22)) + "hobby?!";
+// a constant `hobbyQuestionLength` bound to a number that is the length of `hobby`
+const hobbyQuestionLength = hobbyQuestion.length
