20f83cae288b91a1828219c540e2b69642e82a00
Lucas Gillon <lucas.gillon98@gmail.com>
2022-01-11 14:13:04 +0100

    troisième exercice

---
+++ 02_programming/01_variables/969830e3c3dda891a6b1aeed2709911232022bb1/03_play_with_objects_and_arrays/src/index.js
@@ -0,0 +1,12 @@
+// a constant called `sparta`, bound to an object with a field `check`, with the value of `question` and with the field `response`, with the value of `answer`;
+const sparta = {
+    check: "Spartan, what is your profession?! ",
+    response: "Dev dev dev!",
+}
+
+// a constant called `leader`, bound to the value of the field `check` of the object `sparta`;
+const leader = sparta.check
+
+
+// a constant called `digits`, bound to an array from zero to nine.
+const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
