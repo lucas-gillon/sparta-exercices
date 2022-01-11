2e6810697c6eaa6422034a5fdd47ae1ac0b12c14
Lucas Gillon <lucas.gillon98@gmail.com>
2022-01-11 15:51:47 +0100

    exos 6 &7

--- 02_programming/01_variables/969830e3c3dda891a6b1aeed2709911232022bb1/07_play_with_uppercase/src/index.js
+++ 02_programming/01_variables/969830e3c3dda891a6b1aeed2709911232022bb1/07_play_with_uppercase/src/index.js
@@ -2,7 +2,10 @@ const question = "spartan, what is your profession?!";
 
 // a constant `bigQuestion` bound to a string: "SPARTAN, WHAT IS YOUR PROFESSION?!"
 // using `toUpperCase()` and the previous constant `question`
-
+const bigQuestion = question.toUpperCase();
+console.log(bigQuestion);
 
 // a constant `repeatBigQuestion` bound to a string: "I REPEAT, SPARTAN, WHAT IS YOUR PROFESSION?!"
 // using `toUpperCase()` and the previous constant `question`
+const repeatBigQuestion = "I REPEAT, " + question.toUpperCase();
+console.log(repeatBigQuestion);
