bf3ebac0629044a598e3fa22051572908c6cc463
Lucas Gillon <lucas.gillon98@gmail.com>
2022-01-13 10:48:30 +0100

    jour 3 exo 1

---
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/01_hello_teachers/src/index.js
@@ -0,0 +1,14 @@
+function sayHelloToTeachers(teachers) {
+  // Code the function here.
+  console.log("Hello " + spartaTeachers[0] + "!");
+  console.log("Hello " + spartaTeachers[1] + "!");
+  console.log("Hello " + spartaTeachers[2] + "!");
+  console.log("Hello " + spartaTeachers[3] + "!");
+  console.log("Hello " + spartaTeachers[4] + "!");
+}
+
+// Do not remove last lines, it is for tests
+const spartaTeachers = ["Thomas", "Fenn", "Florian", "Martin", "Louis"];
+module.exports = sayHelloToTeachers;
+
+sayHelloToTeachers();
