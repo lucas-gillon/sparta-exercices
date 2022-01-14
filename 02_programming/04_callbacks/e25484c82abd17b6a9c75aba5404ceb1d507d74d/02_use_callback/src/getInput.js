c0e6092c8ed68dd6d8d22c43254b07bab0e25bb1
Lucas Gillon <lucas.gillon98@gmail.com>
2022-01-14 11:36:57 +0100

    jour 4 exo 2

---
+++ 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/02_use_callback/src/getInput.js
@@ -0,0 +1,9 @@
+// A function getInput that takes two parameters, an input (the first one) and a callback function
+
+function getInput (odin,getInput){
+  return getInput(odin.key);
+}
+
+
+// Leave the line below for tests to work
+(module.exports = getInput);
