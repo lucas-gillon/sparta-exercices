f25bf4d0097a956c45d97c7e33ba3bb59025303a
Lucas Gillon <lucas.gillon98@gmail.com>
2022-01-17 13:31:23 +0100

    jour 5 exo 3

---
+++ 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/03_every_frame_a_painting/src/index.js
@@ -0,0 +1,18 @@
+function canTakeArtPieces(pieces) {
+  const isPainting = (pieces) => pieces.type === "painting";
+  if (pieces.every(isPainting)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// expected output: true
+
+canTakeArtPieces([
+  { piece: "Mona Lisa", type: "painting" },
+  { piece: "Guernica", type: "painting" },
+]);
+
+// Do not remove the following line, it is for tests
+module.exports = canTakeArtPieces;
