afab8b52b577d39fd1cbdb88884debbabd0b37da
Lucas Gillon <lucas.gillon98@gmail.com>
2022-01-17 12:28:20 +0100

    jour 5 exo 2

---
+++ 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/02_filtering_letter_e/src/index.js
@@ -0,0 +1,15 @@
+function theEFilter(sentences) {
+  let filter = sentences.filter((sentences) => sentences.includes("e" )|| sentences.includes("E"));
+  return filter;
+}
+
+theEFilter([
+  "If Youth, throughout all history, had had a champion to stand up for it",
+  "to show a doubting world that a child can think",
+  "and, possibly, do it practically",
+  "ou wouldn't constantly run across folks today who claim that \"a child don't know anything.\"",
+  "A child's brain starts functioning at birth",
+  "and has, amongst its many infant convolutions, thousands of dormant atoms, into which God has put a mystic possibility for noticing an adult's act, and figuring out its purport.",
+]);
+// Do not remove the following line, it is for tests
+module.exports = theEFilter;
