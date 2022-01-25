4598b0e2cdc689ccc346e1e8314d0a24c6497a95
Lucas Gillon <lucas.gillon98@gmail.com>
2022-01-25 15:23:16 +0100

    functions exo 5

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/05_filtering_roles/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/05_filtering_roles/src/index.js
@@ -2,27 +2,49 @@ function filteredRoles(persons, role = "Teacher") {
   // Code the function here.
   let arrayOfPersons = [
     {
-      name: "Thor",
+      name: "Frieda",
+      role: "Teacher",
+    },
+    {
+      name: "John",
+      role: "Teacher",
+    },
+    {
+      name: "Francis",
       role: "Student",
     },
     {
-      name: "Odin",
-      role: "Teacher",
-    },
-    {
-      name: "Freyja",
-      role: "Teacher",
-    },
-    {
-      name: "Loki",
+      name: "Jane",
       role: "Student",
     },
   ];
-  if ((arrayOfPersons.role && arrayOfPersons.persons) === arrayOfPersons.role) {
-    console.log(arrayOfPersons.name && arrayOfPersons.role);
-    return arrayOfPersons.name && arrayOfPersons.role;
+  const myFilteredArray = [];
+  if (role === arrayOfPersons[0].role) {
+    myFilteredArray.push({
+      name: arrayOfPersons[0].name,
+      role: arrayOfPersons[0].role,
+    });
   }
+  if (role === arrayOfPersons[1].role) {
+    myFilteredArray.push({
+      name: arrayOfPersons[1].name,
+      role: arrayOfPersons[1].role,
+    });
+  }
+  if (role === arrayOfPersons[2].role) {
+    myFilteredArray.push({
+      name: arrayOfPersons[2].name,
+      role: arrayOfPersons[2].role,
+    });
+  }
+  if (role === arrayOfPersons[3].role) {
+    myFilteredArray.push({
+      name: arrayOfPersons[3].name,
+      role: arrayOfPersons[3].role,
+    });
+  }
+  return myFilteredArray;
 }
-filteredRoles("Thor", "Student");
+console.log(filteredRoles(["Frieda", "John", "Francis", "Jane"], "Student"));
 // Do not remove lines below, it is for tests
 module.exports = filteredRoles;
