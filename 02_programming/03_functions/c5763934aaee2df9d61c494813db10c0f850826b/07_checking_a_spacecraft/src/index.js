4a384e4458f0a7997141984a6c508ce9f54f074e
Lucas Gillon <lucas.gillon98@gmail.com>
2022-01-25 11:23:11 +0100

    fonctions exo 7

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/07_checking_a_spacecraft/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/07_checking_a_spacecraft/src/index.js
@@ -1,4 +1,22 @@
-// Code here
+const stealthyFuelLevelCheck = (fuelLevel) => {
+  if (fuelLevel > 20) {
+    return "Fuel level: green";
+  } else if (fuelLevel > 10 && fuelLevel < 20) {
+    return "Fuel level: yellow";
+  } else {
+    return "Fuel level: red";
+  }
+};
+
+const stealthyCargoHoldCheck = (cargoHold) => {
+  if (cargoHold.length === 4) {
+    return "Full";
+  } else if (cargoHold.length < 4) {
+    return `Spaces available: ${4 - cargoHold.length}`;
+  } else if (cargoHold.length > 4) {
+    return `Over capacity by ${cargoHold.length - 4} items.`;
+  }
+};
 
 // Do not remove last lines, it is for tests
 
