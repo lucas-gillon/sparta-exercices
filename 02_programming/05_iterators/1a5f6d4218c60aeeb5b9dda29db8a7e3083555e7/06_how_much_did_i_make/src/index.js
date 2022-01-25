bd8bfb869cc47efafcc391719a3575520526aed2
Lucas Gillon <lucas.gillon98@gmail.com>
2022-01-25 12:56:14 +0100

    iterators exo 6

--- 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/06_how_much_did_i_make/src/index.js
+++ 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/06_how_much_did_i_make/src/index.js
@@ -1,5 +1,17 @@
 function howMuchDidIMake(invoices) {
-  // Code here
+  const firstFilter = invoices.filter((invoice) => {
+    return invoice.waiter === "Yourself";
+  });
+
+  const secondFilter = firstFilter.map((invoice) => {
+    return invoice.tip;
+  });
+  let tipsSum = 0;
+  for (let i = 0; i < secondFilter.length; i++) {
+    tipsSum = tipsSum + secondFilter[i];
+  }
+  console.log(tipsSum);
+  return tipsSum;
 }
 
 const invoices = [
@@ -7,6 +19,6 @@ const invoices = [
   { clients: 2, waiter: "Dominique", price: 2200, tip: 50 },
   { clients: 2, waiter: "Yourself", price: 2900, tip: 100 },
 ];
-
+howMuchDidIMake(invoices);
 // Do not remove the following line, it is for tests
 module.exports = howMuchDidIMake;
