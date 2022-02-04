c1b6cb00aee22e70184c87f27d4f0f47ee419945
Lucas Gillon <lucas.gillon98@gmail.com>
2022-02-04 09:52:45 +0100

    unit testing 1 clean git

--- 02_programming/Exos/using-a-submarine/src/index.js
+++ 02_programming/Exos/using-a-submarine/src/index.js
@@ -1,27 +1,17 @@
-// code here
-import { movements } from "./movements.js";
-
-let horizontalPosition = 0;
-let depthPosition = 0;
-let aim = 0;
-
-function submarine(instructions) {
-  for (let i = 0; i < instructions.length; i++) {
-    const words = instructions[i].split(" ");
-    if (words[0] === "forward") {
-      horizontalPosition = horizontalPosition + parseInt(words[1]);
-      depthPosition = depthPosition + parseInt(words[1]) * aim;
-    }
-    if (words[0] === "down") {
-      depthPosition = depthPosition + parseInt(words[1]);
-      aim = aim + parseInt(words[1]);
-    }
-    if (words[0] === "up") {
-      depthPosition = depthPosition - parseInt(words[1]);
-      aim = aim - parseInt(words[1]);
+function counter() {
+  let mockArray = [
+    "Lord of the rings : the return of the king",
+    "Into the wild",
+    "Alien the 8th passenger",
+    "Tombeau des lucioles",
+    "fight club",
+  ];
+  let LOTRcounter = 0;
+  for (let i = 0; i < mockArray.length; i++) {
+    if (mockArray[i].includes("Lord of the rings") === true) {
+      LOTRcounter += 1;
     }
   }
-  console.log(horizontalPosition * depthPosition);
+  return LOTRcounter;
 }
-
-submarine(movements);
+console.log(counter());
