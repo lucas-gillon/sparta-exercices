1e5b185f7c4ac64ebef516c386b220d39814b8e6
Lucas Gillon <lucas.gillon98@gmail.com>
2022-02-10 09:50:06 +0100

    warmup forms & cookies 01

---
+++ 03_frontend_development/04_forms_and_cookies/e5b3d8438cc5eff9e68b205b3efb73619e89654c/01_fundamentals_antiVowels_function/src/sentenceSplitter.ts
@@ -0,0 +1,5 @@
+export const sentenceSplitter = (sentence: string): string[] => {
+  // Code your function sentenceSplitter here
+  return sentence.split(" ");
+};
+console.log(sentenceSplitter("this is a test string"));
