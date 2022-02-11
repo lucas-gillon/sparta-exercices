d8922fc601bc4094c24a39353fecafbc90720cc2
Lucas Gillon <lucas.gillon98@gmail.com>
2022-02-11 09:26:46 +0100

    clean git

--- 03_frontend_development/04_forms_and_cookies/e5b3d8438cc5eff9e68b205b3efb73619e89654c/01_fundamentals_antiVowels_function/src/sentenceJoiner.ts
+++ 03_frontend_development/04_forms_and_cookies/e5b3d8438cc5eff9e68b205b3efb73619e89654c/01_fundamentals_antiVowels_function/src/sentenceJoiner.ts
@@ -1,5 +1,4 @@
 export const sentenceJoiner = (sentence: string[]): string => {
-  // Code the sentenceJoiner function here
   return sentence.join(" ");
 };
 console.log(sentenceJoiner(["this", "is", "a", "test", "string"]));
--- 03_frontend_development/04_forms_and_cookies/e5b3d8438cc5eff9e68b205b3efb73619e89654c/01_fundamentals_antiVowels_function/src/sentenceSplitter.ts
+++ 03_frontend_development/04_forms_and_cookies/e5b3d8438cc5eff9e68b205b3efb73619e89654c/01_fundamentals_antiVowels_function/src/sentenceSplitter.ts
@@ -1,5 +1,4 @@
 export const sentenceSplitter = (sentence: string): string[] => {
-  // Code your function sentenceSplitter here
   return sentence.split(" ");
 };
 console.log(sentenceSplitter("this is a test string"));
