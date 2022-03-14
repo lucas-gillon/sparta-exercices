2bfb0227dd7e4fa0774d5c40248a30a1198e1174
Lucas Gillon <lucas.gillon98@gmail.com>
2022-03-14 20:22:57 +0100

    manual changes

--- 06_fullstack_web_development/01_react_functional_components/fe2008a224b48c8983a2d34ead9d860fe99af1ed/02_product_card/src/components/product-card/index.jsx
+++ 06_fullstack_web_development/01_react_functional_components/fe2008a224b48c8983a2d34ead9d860fe99af1ed/02_product_card/src/components/product-card/index.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import CardHeader from "./header";
+import CardBody from "./body";
 
 const ProductCard = (props) => {
   return (
     <div>
       <CardHeader name={props.product.name} platformLogos={props.product.platforms} />
+      <CardBody
+        coverUrl={props.product.cover.url}
+        releaseDate={props.product.first_release_date}
+        genres={props.product.genres}
+        summary={props.product.summary}
+      />
     </div>
   );
 };
