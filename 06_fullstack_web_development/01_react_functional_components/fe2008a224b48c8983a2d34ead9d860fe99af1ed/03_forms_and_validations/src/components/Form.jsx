bef94fc0c1e42458ca84f8e766e7c9460fd483b7
Lucas Gillon <lucas.gillon98@gmail.com>
2022-03-16 15:51:11 +0100

    exo lundi finis

--- 06_fullstack_web_development/01_react_functional_components/fe2008a224b48c8983a2d34ead9d860fe99af1ed/03_forms_and_validations/src/components/Form.jsx
+++ 06_fullstack_web_development/01_react_functional_components/fe2008a224b48c8983a2d34ead9d860fe99af1ed/03_forms_and_validations/src/components/Form.jsx
@@ -1,5 +1,50 @@
 import React from "react";
 
-const Form = () => "Code here";
+const Form = () => {
+  const [firstName, setFirstName] = React.useState("");
+  const [hasError, setHasError] = React.useState(false);
+  React.useEffect(() => {
+    if (regex.test(firstName) === false) {
+      setHasError(false);
+    } else {
+      setHasError(true);
+    }
+  }, [firstName]);
+
+  let warning;
+  const regex = new RegExp(" ", "gm");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (regex.test(firstName) === false) {
+      warning = <p>bon</p>;
+      console.log(warning.props.children);
+      setHasError(false);
+      return warning;
+    } else {
+      warning = <p>mauvais</p>;
+      console.log(warning.props.children);
+      setHasError(true);
+      return warning;
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label>First Name</label>
+        <input
+          className="form-control"
+          placeholder="First Name"
+          type="text"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        ></input>
+      </div>
+      {hasError ? <p>no spaces allowed</p> : ""}
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+};
 
 export default Form;
--- 06_fullstack_web_development/01_react_functional_components/fe2008a224b48c8983a2d34ead9d860fe99af1ed/04_advanced_form_validation_bonus/src/components/ContactForm/index.jsx
+++ 06_fullstack_web_development/01_react_functional_components/fe2008a224b48c8983a2d34ead9d860fe99af1ed/04_advanced_form_validation_bonus/src/components/ContactForm/index.jsx
@@ -1,7 +1,107 @@
 import React from "react";
 
 const ContactForm = () => {
-  return "Code here!";
+  const [firstName, setFirstName] = React.useState("");
+  const [lastName, setLastName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+
+  const [firstNameError, setfirstNameError] = React.useState(false);
+  const [lastNameError, setlastNameError] = React.useState(false);
+  const [emailError, setEmailError] = React.useState(false);
+
+  const nameRegex = /^[_A-zàâäèéëêïîöôûüù]+((-|\s)*[_A-zàâäèéëêïîöôûüù])*$/gi;
+  // const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+  // const messageRegex = /^.{20,400}$/
+
+  React.useEffect(() => {
+    if (nameRegex.test(firstName) === false) {
+      setfirstNameError(false);
+    } else  {
+      setfirstNameError(true);
+    }
+  }, [firstName]);
+  // React.useEffect(() => {
+  //   if (nameRegex.exec(lastName) === false) {
+  //     setlastNameError(false);
+  //   } else {
+  //     setlastNameError(true);
+  //   }
+  // }, [lastName]);
+  // React.useEffect(() => {
+  //   if (emailRegex.exec(email) === false) {
+  //     setEmailError(false);
+  //   } else {
+  //     setEmailError(true);
+  //   }
+  // }, [email]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label>First Name</label>
+        <input
+          className="form-control"
+          placeholder="First Name"
+          type="text"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        ></input>
+        {
+          firstNameError
+          ?
+          <p>
+            Minimum 1 letter, no special characters expect '-' or space between words
+          </p>
+           :
+           ""
+        }
+      </div>
+      <div className="form-group">
+        <label>Last Name</label>
+        <input
+          className="form-control"
+          placeholder="First Name"
+          type="text"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        ></input>
+        {
+          lastNameError
+          ?
+          <p>
+            Minimum 1 letter, no special characters expect '-' or space between words
+          </p>
+           :
+           ""
+        }
+      </div>
+      <div className="form-group">
+        <label>First Name</label>
+        <input
+          className="form-control"
+          placeholder="First Name"
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
+        {
+          emailError
+          ?
+          <p>
+            Minimum 1 letter, no special characters expect '-' or space between words
+          </p>
+           :
+           ""
+        }
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
 };
 
 export default ContactForm;
